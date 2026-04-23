0520f241673ed61d5e7aa61d6e257f1c1e6c0020
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2026-04-23 06:09:24 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,39 +7,27 @@ int main(int argc, char **argv)
         printf("Usage: %s <binary_file>\n", argv[0]);
         return 1;
     }
-
     t_elf elf;
     t_encryption_info enc_info;
-
-    // Parse the ELF file
     if (parse_elf(argv[1], &elf) != 0)
     {
         printf("File architecture not supported. x86_64 only\n");
         return 1;
     }
-
-    // Validate it's a 64-bit ELF
     if (validate_elf64(&elf) != 0)
     {
         printf("File architecture not supported. x86_64 only\n");
         free_elf(&elf);
         return 1;
     }
-
-    // Find the .text section
     if (find_text_section(&elf, &enc_info.text_offset, &enc_info.text_size) != 0)
     {
         printf("Error: Could not find .text section\n");
         free_elf(&elf);
         return 1;
     }
-
-    // Generate encryption key
     generate_key(enc_info.key);
-
     enc_info.original_entry = elf.ehdr->e_entry;
-
-    // Create the packed binary
     if (create_woody(&elf, "woody", &enc_info) != 0)
     {
         printf("Error: Could not create woody binary\n");
@@ -47,7 +35,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // Print the encryption key
     printf("key_value: ");
     print_key(enc_info.key);
     printf("\n");
