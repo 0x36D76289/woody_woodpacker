1228810e61c069d9349edccb5dbde0c682cdb9ab
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2026-04-23 06:08:37 +0200

    Refactor elf_parser.c

--- src/elf_parser.c
+++ src/elf_parser.c
@@ -11,7 +11,6 @@ int parse_elf(const char *filename, t_elf *elf)
         perror("open");
         return -1;
     }
-
     if (fstat(fd, &st) == -1)
     {
         perror("fstat");
@@ -29,22 +28,16 @@ int parse_elf(const char *filename, t_elf *elf)
     }
 
     close(fd);
-
-    // Basic ELF header validation before casting
     if (elf->size < sizeof(Elf64_Ehdr))
     {
         munmap(elf->data, elf->size);
         return -1;
     }
-
-    // Check ELF magic first
     if (memcmp(elf->data, ELFMAG, SELFMAG) != 0)
     {
         munmap(elf->data, elf->size);
         return -1;
     }
-
-    // Check if it's 64-bit before casting to Elf64_Ehdr
     unsigned char *ident = (unsigned char *)elf->data;
     if (ident[EI_CLASS] != ELFCLASS64)
     {
@@ -57,13 +50,9 @@ int parse_elf(const char *filename, t_elf *elf)
     elf->shdr = (Elf64_Shdr *)((char *)elf->data + elf->ehdr->e_shoff);
 
     if (elf->ehdr->e_shstrndx != SHN_UNDEF)
-    {
         elf->shstrtab = (char *)elf->data + elf->shdr[elf->ehdr->e_shstrndx].sh_offset;
-    }
     else
-    {
         elf->shstrtab = NULL;
-    }
 
     return 0;
 }
@@ -71,61 +60,32 @@ int parse_elf(const char *filename, t_elf *elf)
 void free_elf(t_elf *elf)
 {
     if (elf->data && elf->data != MAP_FAILED)
-    {
         munmap(elf->data, elf->size);
-    }
 }
 
 int validate_elf64(t_elf *elf)
 {
     if (elf->size < sizeof(Elf64_Ehdr))
-    {
         return -1;
-    }
-
-    // Check ELF magic
     if (memcmp(elf->ehdr->e_ident, ELFMAG, SELFMAG) != 0)
-    {
         return -1;
-    }
-
-    // Check if it's 64-bit
     if (elf->ehdr->e_ident[EI_CLASS] != ELFCLASS64)
-    {
         return -1;
-    }
-
-    // Check if it's x86_64
     if (elf->ehdr->e_machine != EM_X86_64)
-    {
         return -1;
-    }
-
-    // Additional validation for ELF structure
     if (elf->ehdr->e_phoff == 0 || elf->ehdr->e_shoff == 0)
-    {
         return -1;
-    }
-
     if (elf->ehdr->e_phoff + (elf->ehdr->e_phnum * sizeof(Elf64_Phdr)) > elf->size)
-    {
         return -1;
-    }
-
     if (elf->ehdr->e_shoff + (elf->ehdr->e_shnum * sizeof(Elf64_Shdr)) > elf->size)
-    {
         return -1;
-    }
-
     return 0;
 }
 
 int find_text_section(t_elf *elf, size_t *offset, size_t *size)
 {
     if (!elf->shstrtab)
-    {
         return -1;
-    }
 
     for (int i = 0; i < elf->ehdr->e_shnum; i++)
     {
